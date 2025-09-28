9300b65b3832ccc0b19e6ec334607b841217e09e
kyryloren <kyrylo@outerlabs.studio>
2025-09-28 16:22:43 -0400

    Update package dependencies: remove @sanity/vision, add sanity-plugin-media, and adjust Sanity configuration to include media plugin.

--- sanity.config.js
+++ sanity.config.js
@@ -4,12 +4,12 @@
  * This configuration is used to for the Sanity Studio that’s mounted on the `/app/studio/[[...tool]]/page.jsx` route
  */
 
-import { visionTool } from '@sanity/vision'
 import { defineConfig } from 'sanity'
 import { structureTool } from 'sanity/structure'
 import { presentationTool } from 'sanity/presentation'
+import { media } from 'sanity-plugin-media'
 
-import { apiVersion, dataset, projectId } from './sanity/env'
+import { dataset, projectId } from './sanity/env'
 import { schema } from './sanity/schemaTypes'
 import { structure } from './sanity/structure'
 
@@ -22,7 +22,7 @@ export default defineConfig({
   schema,
   plugins: [
     structureTool({ structure }),
-    visionTool({ defaultApiVersion: apiVersion }),
+    media(),
     presentationTool({
       previewUrl: {
         initial: process.env.SANITY_STUDIO_PREVIEW_ORIGIN,
